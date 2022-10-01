87c97228cec2aee57e1a26a3980b9aae9bb74307
SatyoshiNakamario <kijekpat@gmail.com>
2022-09-30 22:23:10 -0600

    function does not work as intended yet. no get method

---
+++ video_game_invetory_example.py
@@ -0,0 +1,12 @@
+##Work in Progress. Function does not run right now.##
+
+bag = {'torch': 1, 'sword': 6, 'rupees': 6000000, 'pocket dragon': 1, 'bow': 1, 'arrow': 100}
+
+def displayInventory(inventory):
+    print("inventory:")
+    item_total = 0
+    for k, v in bag.items():
+        item_total = item_total + v.get(inventory, 0)
+    print("This is what you got in that bag:" + str(item_total))
+
+displayInventory(bag)
