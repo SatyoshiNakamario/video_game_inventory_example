20da09c59a3dea40252813f928e73ab6ce4657f0
SatyoshiNakamario <kijekpat@gmail.com>
2022-10-03 19:28:47 -0600

    building out the loot and items into a csv

--- video_game_invetory_example.py
+++ video_game_invetory_example.py
@@ -1,12 +1,48 @@
 ##Work in Progress. Function does not run right now.##
 
+import csv 
+
+#what you always have in your inventory
 bag = {'torch': 1, 'sword': 6, 'rupees': 6000000, 'pocket dragon': 1, 'bow': 1, 'arrow': 100}
 
 def displayInventory(inventory):
-    print("inventory:")
-    item_total = 0
-    for k, v in bag.items():
-        item_total = item_total + v.get(inventory, 0)
-    print("This is what you got in that bag:" + str(item_total))
+    print("This is what you got in that bag:\n")
+    for item in inventory:
+    
+        print(f"{item}:{inventory[item]}")
+
+        print("\n")
+
+
+def add_to_inventory(inventory, added_items):
+
+    #how to create a new item
+    for new_item in inventory:
+        if new_item not in inventory:
+            inventory.update({new_item:0})
+
+    #add item to file
+    for item in inventory:
+        for item in loot:
+            if item ==new_item:
+                inventory += 1
+
+
+#new items from a loot of sorts
+
+loot = ['shield', 'dagger', 'pocket dragon', 'arrow', 'rupees']
+
+add_to_inventory(bag, loot)
+displayInventory(bag)
+
+#function to add the items and their amount in a table
+
+def print_table(inventory, order=none):
+
+
+
+
+
+
 
 displayInventory(bag)
